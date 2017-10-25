a95a7b0bb0d6a33494ba91ee5aea2884b16d304f
neequole <nicole.tibay@johngaltventures.com>
2017-10-25 18:26:45 +0800

    Fix #2 - Check if data folder exists

--- mocker/main.py
+++ mocker/main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from http.server import HTTPServer
 
 from handlers import MainRequestHandlerFactory
@@ -12,12 +13,14 @@ def main():
     parser = argparse.ArgumentParser(description='HTTP data mocker')
     parser.add_argument('data_path', help='The data folder')
     args = parser.parse_args()
-
-    server_address = (HOST, TCP_PORT)
-    print('Starting mocker HTTP server at {}:{}'.format(*server_address))
-    MainRequestHandler = MainRequestHandlerFactory(args.data_path)
-    httpd = HTTPServer(server_address, MainRequestHandler)
-    httpd.serve_forever()
+    if os.path.exists(args.data_path):
+        server_address = (HOST, TCP_PORT)
+        print('Starting mocker HTTP server at {}:{}'.format(*server_address))
+        MainRequestHandler = MainRequestHandlerFactory(args.data_path)
+        httpd = HTTPServer(server_address, MainRequestHandler)
+        httpd.serve_forever()
+    else:
+        print('Folder {} not found'.format(args.data_path))
 
 
 main()
